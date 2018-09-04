20990c90a4862cf99d03a54ae136221146af6027
Janmayjay <37184937+Janmayjay@users.noreply.github.com>
2018-09-04 19:42:25 +0630

    a

--- shopIt/db/orderschema.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require("./connection");
-var Schema = mongoose.Schema;
-var orderSchema = new Schema({
-    "itemId":String,"orderId":String,"name":String,"customerId":String,"customerOrderAddress":String,"productId":Array,"sellerId":String
-});
-var order = mongoose.model("order",orderSchema);
-module.exports = order;
